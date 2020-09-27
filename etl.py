72452218e395c2106b590bb8c5a79aa856145c5a
NeighborWwww <hucstorm@gmail.com>
2020-09-27 15:42:56 -0400

    add docstrings

--- etl.py
+++ etl.py
@@ -6,14 +6,26 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    '''--input:
+        cur: the cursor, can execute the insert queries
+
+        filepath: the filepath of the log dataset. the program will load the .json files in this path and
+        load the information into pandas dataframe.
+
+      --output:
+        void function. all the queries will be execute inside the function.
+
+    The function load the json files in the given filepath and transform them as dataframe. 
+    then, reconstruct the data and using insert queries to load data into 
+    database (artists table and songs table). '''
+
+    #extract data from json file
     df = pd.DataFrame(columns = pd.read_json(filepath[0],lines = True).columns)
     for record in filepath:
         data = pd.read_json(record,lines = True)
         df = df.append(data)
         
-    #    song_data.append([df.values[0][7], df.values[0][8], df.values[0][0], df.values[0][9], df.values[0][5]])
-    #    artist_data.append([df.values[0][0], df.values[0][4], df.values[0][2], df.values[0][1], df.values[0][3]])
-    print(len(df))
+    #transform data into the desire form
     song_data = pd.DataFrame({'song_id':df['song_id'], 'title':df['title'], \
                             'artist_id':df['artist_id'], 'year':df['year'], \
                             'duration':df['duration']})
@@ -21,26 +33,39 @@ def process_song_file(cur, filepath):
                                 'location':df['artist_location'], 'latitude':df['artist_latitude'], \
                                 'longtitude':df['artist_longitude']})
 
+    #load the data into the table
     for i, row in song_data.iterrows():
 
         cur.execute(song_table_insert, list(row))
-        #conn.commit()
     
     # insert artist record
     for i, row in song_data.iterrows():
 
         cur.execute(artist_table_insert, list(row))
-        #conn.commit()
 
 
 def process_log_file(cur, filepath):
-    # open log file
+    '''--input:
+        cur: the cursor, can execute the insert queries
+
+        filepath: the filepath of the songfile dataset. the program will load the .json files in 
+        this path and load the information into pandas dataframe.
+
+       --output:
+        void function. all the queries will be execute inside the function.
+
+    The function load the json files in the given filepath and transform them as dataframe. 'page' = NextSong 
+    need to be filtering out. then, reconstruct the data and using insert queries to load data into database 
+    (time table and users table). select the data from the dimension table, reconstruct the data, and insert 
+    desired data into fact table (songplay table). '''
+    # extract data from json file
     df = pd.DataFrame(columns = pd.read_json(filepath[0],lines = True).columns)
     
     for file in filepath:    
         data = pd.read_json(file, lines = True)
-        
         df = df.append(data)
+
+    #transform data into the desire form
     dt = df[df['page'] == 'NextSong']
     t = dt['ts']
     date = pd.to_datetime(t, unit = 'ms')
@@ -52,11 +77,11 @@ def process_log_file(cur, filepath):
                             'last_name':df['lastName'],'gender':df['gender'], \
                             'level':df['level']})
 
+    #load the data into the tables
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
-    # load user table
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -81,6 +106,16 @@ def process_log_file(cur, filepath):
 
 
 def process_data(cur, conn, filepath, func):
+    '''--input: 
+            cur: the cursor, can execute queries
+            conn: connection, can be use to commit queries if autocommit is not apply
+            filepath: the filepath of dataset
+            func: the function name to call different functions
+       --output:
+            void function
+
+        the function can find each json file path in the directory given, and run the function provided above '''
+        
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -92,11 +127,9 @@ def process_data(cur, conn, filepath, func):
     num_files = len(all_files)
     print('{} files found in {}'.format(num_files, filepath))
 
-    # iterate over files and process
- #   for i, datafile in enumerate(all_files, 1):
+
     func(cur, all_files)
-        #conn.commit()
- #       print('{}/{} files processed.'.format(i, num_files))
+
 
 
 def main():
